ba44ca28cf0635369d8a2b3ffd75f25b3cbd3c12
Chris <netchris@gmail.com>
2015-06-23 20:07:45 -0400

    merging upstream changes

--- LibGearPoints-1.2.lua
+++ LibGearPoints-1.2.lua
@@ -521,33 +521,32 @@ local CUSTOM_ITEM_DATA = {
   [120279] = { 4, 670, "INVTYPE_HEAD", true },
   [119316] = { 4, 670, "INVTYPE_HEAD", true },
   [120278] = { 4, 670, "INVTYPE_HEAD", true },
-
-  -- T18
-  [127953] = { 4, 695, "INVTYPE_CHEST", true},
-  [127962] = { 4, 695, "INVTYPE_CHEST", true},
-  [127963] = { 4, 695, "INVTYPE_CHEST", true},
-
-  [127954] = { 4, 695, "INVTYPE_HAND", true},
-  [127958] = { 4, 695, "INVTYPE_HAND", true},
-  [127964] = { 4, 695, "INVTYPE_HAND", true},
-
-  [127956] = { 4, 695, "INVTYPE_HELM", true},
-  [127959] = { 4, 695, "INVTYPE_HELM", true},
-  [127966] = { 4, 695, "INVTYPE_HELM", true},
-
-  [127955] = { 4, 695, "INVTYPE_LEGS", true},
-  [127960] = { 4, 695, "INVTYPE_LEGS", true},
-  [127965] = { 4, 695, "INVTYPE_LEGS", true},
-
-  [127957] = { 4, 695, "INVTYPE_SHOULDER", true},
-  [127961] = { 4, 695, "INVTYPE_SHOULDER", true},
-  [127967] = { 4, 695, "INVTYPE_SHOULDER", true},
   
-  [127968] = { 4, 705, "INVTYPE_TRINKET", true},
-  [127969] = { 4, 705, "INVTYPE_TRINKET", true},
-  [127970] = { 4, 705, "INVTYPE_TRINKET", true},
+  -- T18
+  [127957] = { 4, 695, "INVTYPE_SHOULDER", true },
+  [127967] = { 4, 695, "INVTYPE_SHOULDER", true },
+  [127961] = { 4, 695, "INVTYPE_SHOULDER", true },
 
+  [127955] = { 4, 695, "INVTYPE_LEGS", true },
+  [127965] = { 4, 695, "INVTYPE_LEGS", true },
+  [127960] = { 4, 695, "INVTYPE_LEGS", true },
 
+  [127956] = { 4, 695, "INVTYPE_HEAD", true },
+  [127966] = { 4, 695, "INVTYPE_HEAD", true },
+  [127959] = { 4, 695, "INVTYPE_HEAD", true },
+
+  [127954] = { 4, 695, "INVTYPE_HAND", true },
+  [127964] = { 4, 695, "INVTYPE_HAND", true },
+  [127958] = { 4, 695, "INVTYPE_HAND", true },
+
+  [127953] = { 4, 695, "INVTYPE_CHEST", true },
+  [127963] = { 4, 695, "INVTYPE_CHEST", true },
+  [127962] = { 4, 695, "INVTYPE_CHEST", true },
+
+  -- T18 trinket tokens (note: slightly higher ilvl)
+  [127969] = { 4, 705, "INVTYPE_TRINKET", true },
+  [127970] = { 4, 705, "INVTYPE_TRINKET", true },
+  [127968] = { 4, 705, "INVTYPE_TRINKET", true },
 }
 
 -- Used to add extra GP if the item contains bonus stats
@@ -709,8 +708,11 @@ function lib:GetValue(item)
     standard_ilvl = 522
   elseif version < 60000 or level_cap == 90 then
     standard_ilvl = 553
-  else
+  elseif version < 60200 then
     standard_ilvl = 680
+    ilvl_denominator = 30
+  else
+    standard_ilvl = 710
     ilvl_denominator = 30
   end
   local multiplier = 1000 * 2 ^ (-standard_ilvl / ilvl_denominator)
